c9a64af879b3b5bb7aebe17cac06ca1b576e68ed
Alec Gibson <aleckgibson@googlemail.com>
2017-03-10 15:04:00 +0000

    Initial commit

---
+++ javascript/cookies_and_headers--phantom.js
@@ -0,0 +1,26 @@
+// ######################################################
+// This is an example module provided by Wraith.
+// Feel free to amend for your own requirements.
+// ######################################################
+module.exports = function (phantom, ready) {
+
+    page.customHeaders = {
+        'GOVUK-ABTest-EducationNavigation': 'B'
+    };
+
+    phantom.addCookie({
+        'name': 'seen_cookie_message',
+        'value': 'yes',
+        'domain': '.gov.uk'
+    });
+
+    phantom.addCookie({
+        'name': 'govuk_not_first_visit',
+        'value': 'yes',
+        'domain': '.gov.uk'
+    });
+
+    phantom.open(phantom.url, function () {
+        setTimeout(ready, 5000);
+    });
+}
